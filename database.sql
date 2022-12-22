4a74a967be5076466a9cc968739b284cd6d10117
Billy Salinas <wsalinas315@gmail.com>
2022-12-22 14:43:29 -0600

    database.sql tables structured

--- database.sql
+++ database.sql
@@ -5,6 +5,61 @@
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "username" VARCHAR (255) UNIQUE NOT NULL,
+    "password" VARCHAR (255) NOT NULL,
+    "profile_image" VARCHAR (255)
+);
+
+-- Animal Database Table
+CREATE TABLE "animal" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (255) UNIQUE NOT NULL,
+    "type" VARCHAR (255) NOT NULL,
+    "subtype" VARCHAR (255) NOT NULL,
+    "family" VARCHAR (255) NOT NULL,
+    "scientific_name" VARCHAR (255) UNIQUE NOT NULL,
+    "length" INT NOT NULL,
+    "length_uom" VARCHAR (255) NOT NULL,
+    "wingspan" INT,
+    "weight" INT NOT NULL,
+    "weight_uom" VARCHAR (255) NOT NULL,
+    "conservation_status" VARCHAR (255) NOT NULL,
+    "description" VARCHAR (1024) NOT NULL,
+    "stock_image" VARCHAR (255) NOT NULL
+);
+
+-- Challenge Table
+CREATE TABLE "challege" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (255) UNIQUE NOT NULL,
+    "description" VARCHAR (255) NOT NULL,
+    "image" VARCHAR (255) NOT NULL,
+    "req_animals" INT NOT NULL
+);
+
+-- Challenge_Animal Junction Table
+CREATE TABLE "challege_animal" (
+    "id" SERIAL PRIMARY KEY,
+    "animal_id" INT NOT NULL REFERENCES "animal",
+    "challenge_id" INT NOT NULL REFERENCES "challenge"
+);
+
+-- Sighting Junction Table
+CREATE TABLE "sighting" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT NOT NULL REFERENCES "user",
+    "animal_id" INT NOT NULL REFERENCES "animal",
+    "date" DATE NOT NULL,
+    "location" VARCHAR (512),
+    "caption" VARCHAR (255),
+    "image" VARCHAR (255)
+);
+
+-- Challenge_User Junction Table
+CREATE TABLE "challenge_user" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT NOT NULL REFERENCES "user",
+    "challenge_id" INT NOT NULL REFERENCES "challenge",
+    "is_complete" BOOLEAN NOT NULL,
+    "sighting_num" INT NOT NULL
 );
