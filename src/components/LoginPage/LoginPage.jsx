aa7587a73ea7835b8077f8f184eaaa1741398208
Billy Salinas <wsalinas315@gmail.com>
2022-12-27 21:09:38 -0600

    logo added to login page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
+import './LoginPage.css';
 
 function LoginPage() {
   const history = useHistory();
+  const logo = require("./CritterCatcherLogo.png");
 
   return (
     <div>
+      {/* App logo */}
+      <img src={logo} />
+      
+      {/* Login form */}
       <LoginForm />
 
+      {/* Registration Link */}
       <center>
         <button
           type="button"
           className="btn btn_asLink"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
+          onClick={() => {history.push('/registration')}}>
           Register New Account
         </button>
       </center>
