62fd5c5f0ab5431bde6bd930b72895b5ab9df8e5
Billy Salinas <wsalinas315@gmail.com>
2023-01-05 15:48:43 -0600

    sightings router PUT for specific sighting id written

--- server/routes/sighting.router.js
+++ server/routes/sighting.router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET all sightings for a user from the database */
 router.get('/:id', (req, res) => {
-  console.log('In server router GET');
+  console.log('In sighting router GET');
   let userID = req.params.id;
   let sightingsQuery = `SELECT * FROM "sightings" WHERE "user_id" = $1;`;
   pool.query(sightingsQuery, [userID]).then((result) => {
@@ -17,7 +17,7 @@ router.get('/:id', (req, res) => {
 
 /* POST a new sighting to the database */
 router.post('/', (req, res) => {
-  console.log('In server router POST');
+  console.log('In sighting router POST');
   let sighting = req.body;
   console.log('Sighting info submitted to server:', sighting);
   let sightingQuery = `INSERT INTO "sighting" ("user_id", "animal_id", "date", "location", "caption", "image")
@@ -37,13 +37,25 @@ router.delete('/:id', (req, res) => {
   let deleteQuery = `DELETE FROM "sightings" WHERE "id" = $1;`;
   pool.query(deleteQuery, [sightingID]).then((result)=> {
     res.sendStatus(200);
-  }). catch((error) => {
+  }).catch((error) => {
     console.log('Error deleting sighting', sightingID, ':', error);
     res.sendStatus(500);
   });
 });
 
-
+/* PUT for updating sightings caption */
+router.put('/:id', (req, res) => {
+  console.log('In sighting router PUT');
+  let updatedCaption = req.body;
+  let sightingID = req.params.id;
+  let editQuery = `UPDATE "sighting" SET "caption" = $1 WHERE "id" = $2;`;
+  pool.query(editQuery, [updatedCaption, sightingID]).then((result) => {
+    res.sendStatus(200);
+  }).catch((error) => {
+    console.log('Error editing sighting', sightingID, ':', error);
+    res.sendStatus(500);
+  });
+});
 
 /* DON'T FORGET TO RUN SIGHTING COUNTS */
 
