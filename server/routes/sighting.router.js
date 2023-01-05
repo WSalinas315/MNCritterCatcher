f6d2161baadd7b14bb06ccf312d9a0d75f877eaa
Billy Salinas <wsalinas315@gmail.com>
2023-01-05 14:25:54 -0600

    wrote sighting.router.js POST

--- server/routes/sighting.router.js
+++ server/routes/sighting.router.js
@@ -14,6 +14,16 @@ router.get('/', (req, res) => {
  */
 router.post('/', (req, res) => {
   // POST route code here
+  let sighting = req.body;
+  console.log('Sighting info submitted to server:', sighting);
+  let sightingQuery = `INSERT INTO "sighting" ("user_id", "animal_id", "date", "location", "caption", "image")
+                       VALUES ($1, $2, $3, $4, $5, $6);`;
+  pool.query(sightingQuery, [sighting.user_id, sighting.animal_id, sighting.date, sighting.location, sighting.caption, sighting.image]).then(result => {
+    res.sendStatus(201);
+  }).catch(error => {
+    console.log('Error posting a new sighting:', error);
+    res.sendStatus(500);
+  });
 });
 
 module.exports = router;
