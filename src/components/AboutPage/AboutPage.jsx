c1c0bf87a5d49efdafd832f9157b347b1c208374
Billy Salinas <wsalinas315@gmail.com>
2023-01-16 15:48:28 -0600

    about page updated

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -2,38 +2,44 @@ import React from 'react';
 import EmailIcon from '@mui/icons-material/Email';
 import { FiMail } from 'react-icons/fi';
 import { FaGithub } from 'react-icons/fa';
+import './AboutPage.css';
 
 function AboutPage() {
   return (
     <div className="container">
-      <div>
+      <div className='about-center'>
 
         <img src='https://avatars.githubusercontent.com/u/111449650?v=4' width="120px" />
         <h1>Billy Salinas</h1>
-        <FaGithub /> https://github.com/WSalinas315
-        <br />
-        <FiMail /> wsalinas315@gmail.com
-        <br />
-        <br />
-        Linkedin
-        <img src='images/about/linkedin.png' width="180px" />
-        <br />
-        Tech Used: <br />
-        React Google Maps API <img src='https://raw.githubusercontent.com/JustFly1984/react-google-maps-api/master/logo.png' width="30px" />
-        JavaScript <img src='https://d2vqpl3tx84ay5.cloudfront.net/500x/tumblr_lsus01g1ik1qies3uo1_400.png' width="30px" />
-        React <img src='https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png' width="30px" />
-        GeoLocation API <br />
-        Redux<br />
-        Saga <br />
-        HTML <br />
-        CSS <br />
-        Node.js <br />
-        Express.js <br />
-        PostgreSQL <br />
-        Material UI <br />
-        Multer Image Upload<br />
-        Passport
 
+        <div className='contacts'>
+          <div className='underline'>
+            Linkedin
+            <img src='images/about/linkedin.png' width="150px" />
+          </div>
+          <div className='vertical-list'>
+            <div><FaGithub className='contact-icon' /> github.com/WSalinas315</div>
+            <div><FiMail className='contact-icon' /> wsalinas315@gmail.com</div>
+          </div>
+        </div>
+
+        <div className='tech-list'>
+          <div className='underline bold'>Tech Used:</div>
+          React Google Maps API<br />
+          GeoLocation API<br />
+          Multer Image Upload<br />
+          React<br />
+          Material UI<br />
+          JavaScript<br />
+          Redux<br />
+          Saga<br />
+          HTML<br />
+          CSS<br />
+          Node.js<br />
+          Express.js<br />
+          PostgreSQL<br />
+          Passport<br />
+        </div>
       </div>
     </div>
   );
